ffec05e36dd2fcd3e175a31864db8b6eddaa63a7
va barbosa <vabarbosa@users.noreply.github.com>
2018-07-25 20:06:51 -0400

    refactor

--- app.js
+++ app.js
@@ -1,20 +1,73 @@
-var request = require('request')
+const request = require('request-promise-native')
 
-const global_headlines = require('./global-weather-notification-headlines')
-const global_details = require('./weatheralertsdetail')
-const tropical_storm = require('./tropicalforecastprojectedpath')
+const globalHeadlines = require('./lib/global-weather-notification-headlines')
+const globalDetails = require('./lib/weather-alerts-detail')
+const tropicalStorm = require('./lib/tropical-forecast-projected-path-source-and-basin-only')
 
-const HOST = "https://api.weather.com"
-const apiKey = process.env.WEATHER_API_KEY
-const baseParams = '?format=json&language=en-US&apiKey=' + apiKey
-const apiOptions = {
-  headers: {
-    'accept': 'application/json', 
-    'gzip': true
+const HOST = 'https://api.weather.com'
+
+const defaultParams = function () {
+  return {
+    qs: {
+      apiKey: process.env.WEATHER_API_KEY,
+      format: 'json',
+      language: 'en-US'
+    },
+    headers: {
+      'User-Agent': 'Request-Promise',
+      'Accept': 'application/json',
+      'gzip': true
+    },
+    json: true // parse the response as JSON
   }
 }
 
-//// Comment out the sample lat/lons you don't want to use or put in your own
+const handleFail = function (err) {
+  // API call failed...
+  console.error(err.hasOwnProperty('message') ? err.message : err)
+}
+
+const callGlobalWeatherNotificationHeadlines = function (lat, lon) {
+  let options = defaultParams()
+  options['uri'] = `${HOST}${globalHeadlines.API}`
+  options.qs['geocode'] = `${lat},${lon}`
+
+  request(options)
+    .then(parsedBody => {
+      let detailKeys = globalHeadlines.handleResponse(parsedBody)
+      if (detailKeys && detailKeys.length > 0) {
+        detailKeys.forEach(detailKey => {
+          callWeatherAlertsDetail(detailKey)
+        })
+      }
+    })
+    .catch(handleFail)
+}
+
+const callWeatherAlertsDetail = function (detailKey) {
+  let options = defaultParams()
+  options['uri'] = `${HOST}${globalDetails.API}`
+  options.qs['alertId'] = detailKey
+
+  request(options)
+    .then(globalDetails.handleResponse)
+    .catch(handleFail)
+}
+
+const callTropicalForecastProjectedPath = function (basin = 'AL', units = 'm', nautical = true, source = 'all') {
+  let options = defaultParams()
+  options['uri'] = `${HOST}${tropicalStorm.API}`
+  options.qs['units'] = units
+  options.qs['basin'] = basin
+  options.qs['nautical'] = nautical
+  options.qs['source'] = source
+
+  request(options)
+    .then(tropicalStorm.handleResponse)
+    .catch(handleFail)
+}
+
+// Comment out the sample lat/lons you don't want to use or put in your own
 // Hawaii
 let lat = '33.40'
 let lon = '-83.42'
@@ -24,84 +77,17 @@ lon = '-68.36907'
 // Boston, MA
 lat = '42.3600'
 lon = '-71.06536'
-// Los Angeles, CA
-lat = '34.040873'
-lon = '-118.482745'
 // Raleigh, NC
 lat = '35.843686'
 lon = '-78.78548'
+// Los Angeles, CA
+lat = '34.040873'
+lon = '-118.482745'
 
 /**
- * Here's an example of setting up a job to look for weather 
- * headlines every 10 minutes. You can set up as many of these 
- * jobs as you like and query different geographies. 
+ * Here's an example of setting up a job to look for weather
+ * headlines every 10 minutes. You can set up as many of these
+ * jobs as you like and query different geographies.
  */
-let to = 1000 * 60 * 10 // 10 minutes
-var jobtimeout = setInterval(()=>callGlobalWeatherNotificationHeadlines(lat, lon), to)
-
-function callGlobalWeatherNotificationHeadlines(lat, lon) {
-  let url = HOST + global_headlines.API + baseParams
-  url += '&geocode=' + lat + '%2C' + lon
-  apiOptions.url = url
-  console.log(url)
-  
-  request(apiOptions, (error, response, body) => {
-    if (!error) {
-      if (response.statusCode == 204) {
-        console.log("204: good request but no alerts in area")
-      } else if (response.statusCode == 200) {
-        res = JSON.parse(body)
-        detailKeys = global_headlines.handleResponse(res)
-        if (detailKeys && detailKeys.length > 0) {
-          detailKeys.forEach(detailKey => {
-            callWeatherAlertsDetail(detailKey)
-          });
-        }
-      } else {
-        console.log("Can't process status code: "+response.statusCode)
-      }
-    } else { // handle error
-      console.log(error)
-    }
-  })
-}
-
-function callWeatherAlertsDetail(detailKey) {
-  let url = HOST + global_details.API + baseParams
-  url += '&alertId=' + detailKey
-  
-  request(apiOptions, (error, response, body) => {
-    if (!error) {
-      if (response.statusCode == 204) {
-        console.log("204: good request but no alerts in area")
-      } else if (response.statusCode == 200) {
-        res = JSON.parse(body)
-        global_details.handleResponse(res)
-      } else {
-        console.log("Can't process status code: "+response.statusCode)
-      }
-    } else { // handle error
-      console.log(error)
-    }
-  })
-}
-
-function callTropicalForecastProjectedPath(basin='AL', units='m', nautical=true, source='all') {
-  let url = HOST + tropical_storm.API + baseParams
-  url += '&units=' + units + '&basin=' + basin + '&nautical=' + nautical + '&source=' + source
-  
-  request(apiOptions, (error, response, body) => {
-    if (!error) {
-      if (response.statusCode == 204) {
-        console.log("204: good request but no alerts in area")
-      } else if (response.statusCode == 200) {
-        res = JSON.parse(body)
-        tropical_storm.handleResponse(res)
-      } else {
-        console.log("Can't process status code: "+response.statusCode)
-      }
-    } else { // handle error
-      console.log(error)
-    }
-  })
-}
+const interval = 1000 * 60 * 10 // 10 minutes
+let jobInterval = setInterval(() => callGlobalWeatherNotificationHeadlines(lat, lon), interval)
